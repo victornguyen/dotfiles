205936529bbf0c6b26c65c030d097faaeeec53e3
Victor Nguyen <vic.nguyen@gmail.com>
2017-06-28 18:36:13 +1000

    Add directory aliases and setup dir stacks

--- zsh/configs/alias.zsh
+++ zsh/configs/alias.zsh
@@ -8,3 +8,26 @@ alias vi="$EDITOR"
 alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
+
+# Directories
+alias l="ls -lah"
+alias lt="ls -laht" # sorted by modified time (most recent first)
+alias ltr="ls -lahtr" # most recent last
+
+# Setup directory stacks
+# http://zsh.sourceforge.net/Intro/intro_6.html
+DIRSTACKSIZE=10
+setopt auto_pushd
+setopt pushd_ignore_dups
+setopt pushdminus
+
+alias d='dirs -v | head -10'
+alias 1='cd -'
+alias 2='cd -2'
+alias 3='cd -3'
+alias 4='cd -4'
+alias 5='cd -5'
+alias 6='cd -6'
+alias 7='cd -7'
+alias 8='cd -8'
+alias 9='cd -9'
