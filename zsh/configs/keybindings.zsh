a8bee0e100d8e06cab7f6b3e69fa8fa0dcd69f69
Victor Nguyen <vic.nguyen@gmail.com>
2017-06-28 17:14:52 +1000

    Add zsh keybindings

---
+++ zsh/configs/keybindings.zsh
@@ -0,0 +1,20 @@
+# Handy key bindings
+
+# Move around command line
+bindkey "^a" beginning-of-line
+bindkey "^e" end-of-line
+bindkey "^f" forward-char
+bindkey "^b" backward-char
+bindkey "^k" kill-line
+bindkey "^d" delete-char
+bindkey "^p" history-search-backward
+bindkey "^n" history-search-forward
+bindkey "^y" accept-and-hold
+bindkey "^w" backward-kill-word
+bindkey "^u" backward-kill-line
+
+# Open current command in Vim
+autoload -z edit-command-line
+zle -N edit-command-line
+bindkey "^x^e" edit-command-line
+
