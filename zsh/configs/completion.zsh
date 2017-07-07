d32ad50dd10d1468b8250f7ef54c555583abcee2
Victor Nguyen <vic.nguyen@gmail.com>
2017-07-07 22:34:22 +1000

    Fix fzf dir completion issue

---
+++ zsh/configs/completion.zsh
@@ -0,0 +1,10 @@
+autoload -U compinit
+# -D flag to prevent caching completions with a .zcompdump file
+# to fix fzf dir completion:
+# https://github.com/junegunn/fzf/issues/553
+# https://stackoverflow.com/a/19313327
+compinit -D
+
+## case-insensitive (all), partial-word and then substring completion
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' \
+    'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
