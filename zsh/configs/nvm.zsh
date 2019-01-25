4c3e9adc34ce7dfe385800e3746c7c2698a8f504
Victor Nguyen <vic.nguyen@gmail.com>
2019-01-25 22:28:44 +1100

    Add nvm activation script

---
+++ zsh/configs/nvm.zsh
@@ -0,0 +1,7 @@
+# Load nvm
+# https://github.com/creationix/nvm
+# TODO: script install steps in setup gist
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
