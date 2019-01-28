71ce5ae40683eaf907b7dc824078e00239b09e44
Victor Nguyen <vic.nguyen@gmail.com>
2019-01-28 12:29:00 +1100

    Remove directory stack aliases
    We have heaps of better traversal methods now...

--- zsh/configs/alias.zsh
+++ zsh/configs/alias.zsh
@@ -22,25 +22,6 @@ else
   alias ltr="ls -lahFt"         # most recent first
 fi
 
-# Directory stacks
-# http://zsh.sourceforge.net/Intro/intro_6.html
-DIRSTACKSIZE=10
-setopt auto_pushd
-setopt pushd_ignore_dups
-setopt pushdminus
-
-# TODO: do we still need these with fasd now?
-alias dd='dirs -v | head -10'
-alias 1='cd -'
-alias 2='cd -2'
-alias 3='cd -3'
-alias 4='cd -4'
-alias 5='cd -5'
-alias 6='cd -6'
-alias 7='cd -7'
-alias 8='cd -8'
-alias 9='cd -9'
-
 # Print each PATH entry on a separate line
 alias path='echo -e ${PATH//:/\\n}'
 
