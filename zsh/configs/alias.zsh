6612d46ecafe5683b478529e03508d7ff448aca3
Victor Nguyen <vic.nguyen@gmail.com>
2019-01-28 12:27:30 +1100

    Replace ls aliases with exa ones

--- zsh/configs/alias.zsh
+++ zsh/configs/alias.zsh
@@ -8,10 +8,19 @@ alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
 
-# Directories
-alias l="ls -lahF"
-alias lt="ls -lahFt" # sorted by modified time (most recent first)
-alias ltr="ls -lahFtr" # most recent last
+# Directory lists
+# Use exa if available
+if [[ -x "$(command -v exa)" ]]; then
+  alias l="exa -la"             # long list
+  alias lg="exa -la --git"      # include git status
+  alias lt="exa -la -s=mod"     # most recent last
+  alias ltr="exa -lar -s=mod"   # most recent first
+  alias ltree="exa -a -T -L=2"  # tree view
+else
+  alias l="ls -lahF"            # long list
+  alias lt="ls -lahFtr"         # most recent last
+  alias ltr="ls -lahFt"         # most recent first
+fi
 
 # Directory stacks
 # http://zsh.sourceforge.net/Intro/intro_6.html
