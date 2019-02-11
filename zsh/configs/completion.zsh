065a2919458802249b414d175c3485930b35c06c
Victor Nguyen <vic.nguyen@gmail.com>
2019-02-11 22:28:07 +1100

    Highlight zsh completion options when selected

--- zsh/configs/completion.zsh
+++ zsh/configs/completion.zsh
@@ -5,5 +5,5 @@
 autoload -U compinit -D
 
 ## case-insensitive (all), partial-word and then substring completion
-zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' \
+zstyle ':completion:*' menu select matcher-list 'm:{a-zA-Z}={A-Za-z}' \
     'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
