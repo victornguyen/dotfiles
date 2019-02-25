57d12b5d3814642c3cad3ce2325c43c56641dd5f
Victor Nguyen <vic.nguyen@gmail.com>
2019-02-25 23:56:21 +1100

    Improve shell startup time 🚀
    Do not invoke nvm until first node/npm/nvm command is run.

--- zsh/configs/nvm.zsh
+++ zsh/configs/nvm.zsh
@@ -2,5 +2,15 @@
 # https://github.com/creationix/nvm
 
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# nvm sloooows down shell load time
+# Do not load nvm until first node/npm/nvm invocation
+# https://github.com/creationix/nvm/issues/1277#issuecomment-455809893
+nvm_load () {
+  . $NVM_DIR/nvm.sh
+  . $NVM_DIR/bash_completion
+}
+
+alias node='unalias nvm; unalias node; unalias npm; nvm_load; node $@'
+alias npm='unalias nvm; unalias node; unalias npm; nvm_load; npm $@'
+alias nvm='unalias nvm; unalias node; unalias npm; nvm_load; nvm $@'
