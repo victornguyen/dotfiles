93e0655020d35f2ff5169a9075ebcd2631ff5e7c
Victor Nguyen <vic.nguyen@gmail.com>
2019-02-28 23:38:37 +1100

    Make first yarn invocation lazy load nvm

--- zsh/configs/nvm.zsh
+++ zsh/configs/nvm.zsh
@@ -4,13 +4,14 @@
 export NVM_DIR="$HOME/.nvm"
 
 # nvm sloooows down shell load time
-# Do not load nvm until first node/npm/nvm invocation
+# Do not load nvm until first node/npm/yarn/nvm invocation
 # https://github.com/creationix/nvm/issues/1277#issuecomment-455809893
 nvm_load () {
   . $NVM_DIR/nvm.sh
   . $NVM_DIR/bash_completion
 }
 
-alias node='unalias nvm; unalias node; unalias npm; nvm_load; node $@'
-alias npm='unalias nvm; unalias node; unalias npm; nvm_load; npm $@'
-alias nvm='unalias nvm; unalias node; unalias npm; nvm_load; nvm $@'
+alias node='unalias nvm; unalias node; unalias npm; unalias yarn; nvm_load; node $@'
+alias npm='unalias nvm; unalias node; unalias npm; unalias yarn; nvm_load; npm $@'
+alias yarn='unalias nvm; unalias node; unalias npm; unalias yarn; nvm_load; yarn $@'
+alias nvm='unalias nvm; unalias node; unalias npm; unalias yarn; nvm_load; nvm $@'
