75a70345116cce943b2ec027f6b40de0e80c011c
Victor Nguyen <vic.nguyen@gmail.com>
2019-05-28 10:48:29 +1000

    Add git status file selector fzf helper to shell
    Also add shell key bindings for fzf gf and gb helpers.
    Add fo git alias to select and open files from git status.

--- zsh/configs/fzf.zsh
+++ zsh/configs/fzf.zsh
@@ -12,6 +12,26 @@ export FZF_DEFAULT_COMMAND='rg --files --hidden --glob "!.git/*"'
 # Apply the default fzf command to CTRL-T as well
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
+# Helper for bind-git-helper()
+join-lines() {
+  local item
+  while read item; do
+    echo -n "${(q)item} "
+  done
+}
+
+# Bind keys g[x] to fzf-g[x]-widget in zsh
+bind-git-helper() {
+  local c
+  for c in $@; do
+    eval "fzf-g$c-widget() { local result=\$(g$c | join-lines); zle reset-prompt; LBUFFER+=\$result }"
+    eval "zle -N fzf-g$c-widget"
+    eval "bindkey '^g^$c' fzf-g$c-widget"
+  done
+}
+bind-git-helper f b
+unset -f bind-git-helper
+
 # base16-oceanicnext fzf colour scheme
 # https://github.com/nicodebo/base16-fzf
 _gen_fzf_default_opts() {
