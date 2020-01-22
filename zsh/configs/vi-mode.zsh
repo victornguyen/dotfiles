8bf539806cdf7438ce0e2cd5459b53a0c7d1b2ff
Victor Nguyen <vic.nguyen@gmail.com>
2020-01-22 16:14:29 +1100

    Configure zsh vi-mode mostly so it's not sluggish

---
+++ zsh/configs/vi-mode.zsh
@@ -0,0 +1,11 @@
+# Enable vi-mode
+bindkey -v
+
+# Kill the lag
+export KEYTIMEOUT=1
+
+# Tweak prompt appearance in vi-mode
+export PURE_PROMPT_VICMD_SYMBOL='%F{yellow}❮%f'
+
+# laurenkt/zsh-vimto config
+export VIMTO_COLOR_NORMAL_BACKGROUND=yellow
