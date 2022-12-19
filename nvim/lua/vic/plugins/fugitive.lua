ea2510db95cb67fa9373dd41b740811758026dc6
Victor Nguyen <vic.nguyen@gmail.com>
2022-12-20 10:11:22 +1100

    Fix fugitive diff command

--- nvim/lua/vic/plugins/fugitive.lua
+++ nvim/lua/vic/plugins/fugitive.lua
@@ -1,7 +1,7 @@
 local m = require('vic.utils')
 
 m.map('n', '<leader>gb', ':Git blame<cr>')
-m.map('n', '<leader>gd', ':Gitdiffsplit<cr>')
+m.map('n', '<leader>gd', ':Gdiffsplit<cr>')
 m.map('n', '<leader>gS', ':Git<cr>')
 m.map('n', '<leader>G', ':Git status<cr>')
 m.map('n', '<leader>gl', ':0Gclog<cr>')
