b195412ae5ee3a079c9205c8a38880f892c80c6b
Victor Nguyen <vic.nguyen@gmail.com>
2023-01-25 10:20:38 +1100

    Use telescope branch to enable a working builtin.current_buffer_fuzzy_find

--- nvim/lua/vic/plugins.lua
+++ nvim/lua/vic/plugins.lua
@@ -107,7 +107,7 @@ return packer.startup(function(use)
   -- Telescope
   use({
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.0',
+    branch = '0.1.x',
     requires = { 'nvim-lua/plenary.nvim' },
     config = function()
       require('vic.plugins.telescope')
