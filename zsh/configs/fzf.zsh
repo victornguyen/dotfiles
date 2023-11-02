7b88f45fa8916b61c48ecf790f5237a0e588ba68
Victor Nguyen <vic.nguyen@gmail.com>
2023-11-02 23:06:41 +1100

    theme: update fzf use catppuuccin colours

--- zsh/configs/fzf.zsh
+++ zsh/configs/fzf.zsh
@@ -32,10 +32,8 @@ bind-git-helper() {
 bind-git-helper f b
 unset -f bind-git-helper
 
-export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS' --color=fg:#c0caf5,bg:#24283b,hl:#f7768e --color=fg+:#73daca,bg+:#33467c,hl+:#f7768e --color=info:#73daca,prompt:#bb9af7,pointer:#73daca --color=marker:#c0caf5,spinner:#73daca,header:#7aa2f7'
-
-# export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS' 
-# --color=fg:#c0caf5,bg:#1a1b26,hl:#bb9af7
-# --color=fg+:#c0caf5,bg+:#1a1b26,hl+:#7dcfff
-# --color=info:#7aa2f7,prompt:#7dcfff,pointer:#7dcfff 
-# --color=marker:#9ece6a,spinner:#9ece6a,header:#9ece6a'
+# catppuuccin/fzf
+export FZF_DEFAULT_OPTS=" \
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
