7b0ea0a62c6b532d8400eccddb4221e5483718a7
Victor Nguyen <vic.nguyen@gmail.com>
2023-11-07 15:17:21 +1100

    nvim: update fugitive binds

--- nvim/lua/vic/plugins/fugitive.lua
+++ nvim/lua/vic/plugins/fugitive.lua
@@ -1,17 +1,19 @@
 return {
   'tpope/vim-fugitive',
   dependencies = {
-    'tpope/vim-rhubarb',    -- Github support
+    'tpope/vim-rhubarb', -- Github support!
     'tommcdo/vim-fubitive', -- Bitbucket support
   },
   config = function()
     local m = require('vic.utils')
 
     m.map('n', '<leader>gb', ':Git blame<cr>')
-    m.map('n', '<leader>gd', ':Gdiffsplit<cr>')
-    m.map('n', '<leader>gS', ':Git<cr>')
-    m.map('n', '<leader>G', ':Git status<cr>')
+    m.map('n', '<leader>gd', ':Gdiffsplit!<cr>')
+    m.map('n', '<leader>gs', ':Git<cr>')
     m.map('n', '<leader>gl', ':0Gclog<cr>')
     m.map('n', '<leader>go', ':GBrowse<cr>')
+
+    m.map('n', 'gh', '<cmd>diffget //2<cr>')
+    m.map('n', 'gl', '<cmd>diffget //3<cr>')
   end,
 }
