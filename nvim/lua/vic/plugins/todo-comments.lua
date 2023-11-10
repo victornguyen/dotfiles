3c3bcc9e94dba9ae82eaa4d90453ca95a5b10cdc
Victor Nguyen <vic.nguyen@gmail.com>
2023-11-10 18:51:58 +1100

    nvim: add todo-comments plugin

---
+++ nvim/lua/vic/plugins/todo-comments.lua
@@ -0,0 +1,31 @@
+return {
+  'folke/todo-comments.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  opts = {
+    signs = true, -- show icons in the signs column
+    keywords = {
+      FIX = {
+        icon = ' ', -- icon used for the sign, and in search results
+        color = 'error', -- can be a hex color, or a named color (see below)
+        alt = { 'FIXME', 'BUG', 'FIXIT', 'ISSUE' }, -- a set of other keywords that all map to this FIX keywords
+        -- signs = false, -- configure signs for some keywords individually
+      },
+      TODO = { icon = ' ', color = 'info' },
+      HACK = { icon = ' ', color = 'warning' },
+      WARN = { icon = ' ', color = 'warning', alt = { 'WARNING', 'XXX' } },
+      PERF = { icon = ' ', alt = { 'OPTIM', 'PERFORMANCE', 'OPTIMIZE' } },
+      NOTE = { icon = ' ', color = 'hint', alt = { 'INFO' } },
+      TEST = { icon = '⏲ ', color = 'test', alt = { 'TESTING', 'PASSED', 'FAILED' } },
+    },
+  },
+  config = function(_, opts)
+    local m = require('vic.utils')
+    local todo_comments = require('todo-comments')
+
+    -- Binds for navigating comment keywords in a buffer
+    m.map('n', ']t', todo_comments.jump_next, { desc = 'Next comment keyword' })
+    m.map('n', '[t', todo_comments.jump_prev, { desc = 'Previous comment keyword' })
+
+    todo_comments.setup(opts)
+  end,
+}
