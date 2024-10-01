59264316a7d4103c86d0e9a763e0964e0df9a40c
Victor Nguyen <vic.nguyen@gmail.com>
2024-10-01 22:39:27 +1000

    nvim: update fugitive to support enterprise github instances
    Defined in a local file

--- nvim/lua/vic/plugins/fugitive.lua
+++ nvim/lua/vic/plugins/fugitive.lua
@@ -1,3 +1,21 @@
+local function read_file(file_path)
+  local file = io.open(file_path, 'r')
+  if not file then
+    return nil, 'File not found: ' .. file_path
+  end
+  local content = file:read('*all')
+  file:close()
+  return content
+end
+
+local function parse_urls(content)
+  local urls = {}
+  for url in content:gmatch('[^\r\n]+') do
+    table.insert(urls, url)
+  end
+  return urls
+end
+
 return {
   'tpope/vim-fugitive',
   dependencies = {
@@ -15,5 +33,22 @@ return {
 
     m.map('n', 'gh', '<cmd>diffget //2<cr>')
     m.map('n', 'gl', '<cmd>diffget //3<cr>')
+
+    -- Define the path to the .local file
+    -- Example (one url per line):
+    -- https://github.company.com
+    -- https://github.domain.com
+    local local_file_path = vim.fn.expand('~/.github_enterprise_urls')
+
+    -- Read the file content
+    local content, err = read_file(local_file_path)
+    if not content then
+      print('Error reading file: ' .. err)
+    else
+      -- Parse the URLs from the file content
+      local github_enterprise_urls = parse_urls(content)
+      -- Set the URLs as a Vim variable
+      vim.api.nvim_set_var('github_enterprise_urls', github_enterprise_urls)
+    end
   end,
 }
