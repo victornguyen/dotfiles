0dc4642efe7f00d4389abbae2c61314eeffc2b10
Victor Nguyen <vic.nguyen@gmail.com>
2025-02-01 12:58:02 +1100

    nvim: tweak chatgpt plugin settings

--- nvim/lua/vic/plugins/chatgpt.lua
+++ nvim/lua/vic/plugins/chatgpt.lua
@@ -14,7 +14,12 @@ return {
       api_key_cmd = 'gpg --decrypt ' .. home .. '/secret.txt.gpg',
       openai_params = {
         model = 'gpt-4o',
+        frequency_penalty = 0,
+        presence_penalty = 0,
         max_tokens = 4095,
+        temperature = 0.2,
+        top_p = 0.1,
+        n = 1,
       },
     })
   end,
