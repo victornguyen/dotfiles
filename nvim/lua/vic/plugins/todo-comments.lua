9ed7fb9219056118430458222b6233177e7faa65
Victor Nguyen <vic.nguyen@gmail.com>
2025-06-26 16:16:45 +1000

    chore(nvim): make colour of NOTE todo comments less like TODO
    In current theme...

--- nvim/lua/vic/plugins/todo-comments.lua
+++ nvim/lua/vic/plugins/todo-comments.lua
@@ -14,7 +14,7 @@ return {
       HACK = { icon = ' ', color = 'warning' },
       WARN = { icon = ' ', color = 'warning', alt = { 'WARNING', 'XXX' } },
       PERF = { icon = ' ', alt = { 'OPTIM', 'PERFORMANCE', 'OPTIMIZE' } },
-      NOTE = { icon = ' ', color = 'hint', alt = { 'INFO' } },
+      NOTE = { icon = ' ', color = 'default', alt = { 'INFO' } },
       TEST = { icon = '⏲ ', color = 'test', alt = { 'TESTING', 'PASSED', 'FAILED' } },
     },
   },
