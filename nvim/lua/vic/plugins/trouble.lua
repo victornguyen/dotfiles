d3cc582486a9ce7b8ce15687cf7e095c333d4ff2
Victor Nguyen <vic.nguyen@gmail.com>
2025-07-01 08:56:07 +1000

    chore(nvim): add Trouble key binds

--- nvim/lua/vic/plugins/trouble.lua
+++ nvim/lua/vic/plugins/trouble.lua
@@ -2,4 +2,21 @@ return {
   'folke/trouble.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   opts = {},
+  keys = {
+    {
+      '<leader>xx',
+      '<cmd>Trouble diagnostics toggle<cr>',
+      desc = 'Diagnostics (Trouble)',
+    },
+    {
+      '<leader>xX',
+      '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+      desc = 'Buffer Diagnostics (Trouble)',
+    },
+    {
+      '<leader>xt',
+      '<cmd>Trouble todo toggle filter.buf=0<cr>',
+      desc = 'TODO List Buffer (Trouble)',
+    },
+  },
 }
