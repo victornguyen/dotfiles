27b00784e95c7a3762e6df4398746c7306cc11dc
Victor Nguyen <vic.nguyen@gmail.com>
2025-07-01 08:59:33 +1000

    chore(nvim): replace use of deprecated vim.buf_get_clients method

--- nvim/lua/vic/plugins/lualine/components.lua
+++ nvim/lua/vic/plugins/lualine/components.lua
@@ -3,7 +3,7 @@ local M = {}
 M.lsp_clients = function()
   local bufnr = vim.api.nvim_get_current_buf()
 
-  local clients = vim.lsp.buf_get_clients(bufnr)
+  local clients = vim.lsp.get_clients(bufnr)
   if next(clients) == nil then
     return ''
   end
