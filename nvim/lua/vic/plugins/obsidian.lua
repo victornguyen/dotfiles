4a757b16811177431c02c2dfc7e26813cb46ca33
Victor Nguyen <vic.nguyen@gmail.com>
2025-08-14 17:10:24 +1000

    chore(nvim): add new obsidian binds and tweak checkbox order

--- nvim/lua/vic/plugins/obsidian.lua
+++ nvim/lua/vic/plugins/obsidian.lua
@@ -42,31 +42,52 @@ return {
       default_tags = { 'daily-notes' },
     },
     preferred_link_style = 'markdown',
+    checkbox = {
+      order = { ' ', 'x', '~', '!', '>' },
+    },
   },
   keys = {
     -- Essential daily workflow
-    { '<leader>ot', '<cmd>Obsidian today<cr>',        desc = 'Obsidian: Open today' },
+    { '<leader>ot', '<cmd>Obsidian today<cr>', desc = 'Obsidian: Open today' },
     { '<leader>op', '<cmd>Obsidian quick_switch<cr>', desc = 'Obsidian: Quick switch' },
-    { '<leader>os', '<cmd>Obsidian search<cr>',       desc = 'Obsidian: Search notes' },
-    { '<leader>on', '<cmd>Obsidian new<cr>',          desc = 'Obsidian: New note' },
+    { '<leader>os', '<cmd>Obsidian search<cr>', desc = 'Obsidian: Search notes' },
+    { '<leader>on', '<cmd>Obsidian new<cr>', desc = 'Obsidian: New note' },
 
     -- Navigation and links
-    { '<leader>ob', '<cmd>Obsidian backlinks<cr>',    desc = 'Obsidian: Show backlinks' },
-    { '<leader>ol', '<cmd>Obsidian links<cr>',        desc = 'Obsidian: Show links' },
+    { '<leader>ob', '<cmd>Obsidian backlinks<cr>', desc = 'Obsidian: Show backlinks' },
+    { '<leader>ol', '<cmd>Obsidian links<cr>', desc = 'Obsidian: Show links' },
 
     -- Additional useful commands
-    { '<leader>og', '<cmd>Obsidian tags<cr>',         desc = 'Obsidian: Browse tags' },
-    { '<leader>oy', '<cmd>Obsidian yesterday<cr>',    desc = 'Obsidian: Open yesterday' },
-    { '<leader>om', '<cmd>Obsidian tomorrow<cr>',     desc = 'Obsidian: Open tomorrow' },
-    { '<leader>od', '<cmd>Obsidian dailies<cr>',      desc = 'Obsidian: Browse dailies' },
-    { '<leader>oc', '<cmd>Obsidian toc<cr>',          desc = 'Obsidian: Table of contents' },
+    { '<leader>og', '<cmd>Obsidian tags<cr>', desc = 'Obsidian: Browse tags' },
+    { '<leader>oy', '<cmd>Obsidian yesterday<cr>', desc = 'Obsidian: Open yesterday' },
+    { '<leader>om', '<cmd>Obsidian tomorrow<cr>', desc = 'Obsidian: Open tomorrow' },
+    { '<leader>od', '<cmd>Obsidian dailies<cr>', desc = 'Obsidian: Browse dailies' },
+    { '<leader>oc', '<cmd>Obsidian toc<cr>', desc = 'Obsidian: Table of contents' },
     {
       '<leader>oe',
       '<cmd>Obsidian extract_note<cr>',
       desc = 'Obsidian: Extract to new note',
       mode = 'v',
     },
+    {
+      '<leader>ol',
+      '<cmd>Obsidian link_new<cr>',
+      desc = 'Obsidian: Create link from selection',
+      mode = 'v',
+    },
     { '<leader>or', '<cmd>Obsidian rename<cr>', desc = 'Obsidian: Rename note' },
-    { '<leader>oO', '<cmd>Obsidian open<cr>',   desc = 'Obsidian: Open in app' },
+    { '<leader>oO', '<cmd>Obsidian open<cr>', desc = 'Obsidian: Open in app' },
+    {
+      '<leader>ok',
+      function()
+        local builtin = require('telescope.builtin')
+        builtin.live_grep({
+          search_dirs = { vim.fn.expand('%:p') },
+          default_text = '- \\[ \\]',
+          prompt_title = 'Find Unchecked Checkboxes',
+        })
+      end,
+      desc = 'Obsidian: Find unchecked checkboxes',
+    },
   },
 }
