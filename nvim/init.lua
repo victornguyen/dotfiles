a500bbca9dd6c7d0e14e7a807aaae3982d050552
Victor Nguyen <vic.nguyen@gmail.com>
2026-03-07 18:10:00 +1100

    fix(nvim): replace deprecated vim.loop with vim.uv
    vim.loop is deprecated in Neovim 0.10+ in favor of vim.uv.

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,7 +7,7 @@ require('vic.config')
 require('vic.options')
 
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
   vim.fn.system({
     'git',
     'clone',
